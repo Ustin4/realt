7080bc9815d4a67fdc78bf6e5bffef01a160ec62
Ustin4 <Mortyvp@yandex.by>
2024-11-10 01:42:18 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className={'min-h-screen mb-4 bg-cover bg-center flex-initial items-center w-full overflow-hidden'}>
+            
+        </div>
+    );
+};
+
+export default Header;
