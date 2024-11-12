d38c9c89a9825c60e342aee15b859f1ce48a2b9e
Ustin4 <Mortyvp@yandex.by>
2024-11-12 21:48:41 +0300

    add about and navBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header.jsx";
+import About from "./components/About.jsx";
 
 const App = () => {
     return (
         <div className='w-full overflow-hidden'>
             <Header />
+            <About />
         </div>
     );
 };
